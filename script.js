10dc2baf763fb779ce916a7e9a980189727a5da1
Gil Alves <gilderlanioalves00@gmail.com>
2024-03-03 16:45:56 -0300

    Impletmentação CSS
    Foi feito a implementação do style.css. E também dando inicio da implementação do script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Testando");
+
+let sliderElement = document.querySelector("#slider");
+let buttonElement = document.querySelector("#button-crt");
+
+let sizePassword = document.querySelector("#valor-tamanho");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password");
+
+let charset = "qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM!@#$%¨&*";
+let novaSenha = "";
