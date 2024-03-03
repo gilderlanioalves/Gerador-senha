0fb6c23258c3b98d05b57c67d68943a8d3bf0ef0
Gil Alves <gilderlanioalves00@gmail.com>
2024-03-03 17:27:00 -0300

    Implementação JS
    Concluida a implementação do scritp.js e foi finalizada toda sintaxe de funcionamento projeto.

--- script.js
+++ script.js
@@ -9,3 +9,29 @@ let containerPassword = document.querySelector("#container-password");
 
 let charset = "qwertyuiopasdfghjklçzxcvbnmQWERTYUIOPASDFGHJKLÇZXCVBNM!@#$%¨&*";
 let novaSenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
+
+slider.oninput= function(){
+    sizePassword.innerHTML=this.value;
+}
+
+
+function generationPass(){
+    let pass = '';
+    for (let i = 0, n = charset.length; i < sliderElement.value ; i++){
+        pass += charset.charAt(Math.floor(Math.random() * n));
+    }
+    
+    containerPassword.classList.remove("hide");
+    password.innerHTML = pass;
+    novaSenha = pass;
+}
+
+function copyClickPass(){
+    alert("Senha Copiada!");
+    navigator.clipboard.writeText(novaSenha);
+}
+
+
+//@gilderlanio_alves
